8e7c16fe91f5caac3265c0aebfd511c8cdf14b26
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-01 22:09:47 +0530

    Added : Created findMaximum method for checking max number which is present second position

--- main/java/com/testmaximum/TestMaximum.java
+++ main/java/com/testmaximum/TestMaximum.java
@@ -10,9 +10,9 @@ public class TestMaximum {
     }
 
     public static Integer findMaximum(Integer xInt, Integer yInt, Integer zInt) {
-        Integer max = xInt;
-        if(yInt.compareTo(max) > 0)
-            max = yInt;
+        Integer max = yInt;
+        if(xInt.compareTo(max) > 0)
+            max = xInt;
         if(zInt.compareTo(max) > 0)
             max = zInt;
         return max;
