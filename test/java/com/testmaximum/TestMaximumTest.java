c6bc2c9f728f887a54347e671e4383e2d7e2f283
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-01 22:21:11 +0530

    Tested : Test data has max number in third position

--- test/java/com/testmaximum/TestMaximumTest.java
+++ test/java/com/testmaximum/TestMaximumTest.java
@@ -5,18 +5,17 @@ import org.junit.jupiter.api.Test;
 
 public class TestMaximumTest {
     @Test
-    public void givenMaxNumber_AtSecondPosition_ShouldReturnTrue(){
+    public void givenMaxNumber_AtThirdPosition_ShouldReturnTrue(){
         TestMaximum userNumbers = new TestMaximum();
-        Integer result = userNumbers.findMaximum(2,3,1);
+        Integer result = userNumbers.findMaximum(2,1,3);
         boolean max = (result==3) ? true : false;
         Assertions.assertTrue(max);
     }
-
-
+    
     @Test
-    public void givenMaxNumber_NotAtSeconndPosition_ShouldReturnFalse(){
+    public void givenMaxNumber_NotAtThirdPosition_ShouldReturnFalse(){
         TestMaximum userNumbers = new TestMaximum();
-        Integer result = userNumbers.findMaximum(10,6,5);
+        Integer result = userNumbers.findMaximum(10,20,6);
         boolean max = (result==6) ? true : false;
         Assertions.assertFalse(max);
     }
