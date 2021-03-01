dbba85e6024cdcb47cb7274c3df53d59f2821a8a
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-02 02:05:54 +0530

    Tested : Wrote Junit test to test data of different generic type and find maximum from them

--- test/java/com/testmaximum/TestMaximumTest.java
+++ test/java/com/testmaximum/TestMaximumTest.java
@@ -3,19 +3,52 @@ package com.testmaximum;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class TestMaximumTest {
+public class TestMaximumTest{
+
     @Test
-    public void givenMaxString_AtThirdPosition_ShouldReturnTrue(){
+    public void givenIntegerMaxNumber_WhenProper_ShouldReturnTrue(){
+        TestMaximum userNumbers = new TestMaximum();
+        Integer result =  userNumbers.findMaximum(3,2,1);
+        boolean max = (result==3) ? true : false;
+        Assertions.assertTrue(max);
+    }
+
+    @Test
+    public void givenIntegerMaxNumber_WhenNotProper_ShouldReturnFalse(){
+        TestMaximum userNumbers = new TestMaximum();
+        Integer result =  userNumbers.findMaximum(6,10,5);
+        boolean max = (result==6) ? true : false;
+        Assertions.assertFalse(max);
+    }
+
+    @Test
+    public void givenFloatMaxNumber_WhenProper_ShouldReturnTrue(){
+        TestMaximum userNumbers = new TestMaximum();
+        Float result =  userNumbers.findMaximum( 3.1f,2.1f,1.1f);
+        boolean max = (result==3.1f) ? true : false;
+        Assertions.assertTrue(max);
+    }
+
+    @Test
+    public void givenFloatMaxNumber_WhenNotProper_ShouldReturnFalse(){
+        TestMaximum userNumbers = new TestMaximum();
+        Float result =  userNumbers.findMaximum(2.1f,3.1f,1.1f);
+        boolean max = (result==2.1f) ? true : false;
+        Assertions.assertFalse(max);
+    }
+
+    @Test
+    public void givenMaxString_WhenProper_ShouldReturnTrue(){
         TestMaximum userString = new TestMaximum();
-        String result = userString.findMaximum( "Apple","Banana","Peach");
+        String result =  userString.findMaximum( "Peach","Banana","Apple");
         boolean max = (result.equals("Peach")) ? true : false;
         Assertions.assertTrue(max);
     }
     @Test
-    public void givenMaxString_NotAtThirdPosition_ShouldReturnFalse(){
+    public void givenMaxString_WhenNotProper_ShouldReturnFalse(){
         TestMaximum userString = new TestMaximum();
-        String result = userString.findMaximum("Apple","Peach","Banana");
-        boolean max = (result.equals("Banana")) ? true : false;
+        String result =  userString.findMaximum("Apple","Peach","Banana");
+        boolean max = (result.equals("Apple")) ? true : false;
         Assertions.assertFalse(max);
     }
 }
