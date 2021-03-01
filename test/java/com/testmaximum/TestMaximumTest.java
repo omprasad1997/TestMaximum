a454e37a157a8cb4ba90e0a47160af9fd315cdd9
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-02 00:48:24 +0530

    Tested : Wrote Junit test to test data has max string in second position

--- test/java/com/testmaximum/TestMaximumTest.java
+++ test/java/com/testmaximum/TestMaximumTest.java
@@ -5,18 +5,18 @@ import org.junit.jupiter.api.Test;
 
 public class TestMaximumTest {
     @Test
-    public void givenMaxString_AtFirstPosition_ShouldReturnTrue(){
+    public void givenMaxString_AtSecondPosition_ShouldReturnTrue(){
         TestMaximum userString = new TestMaximum();
-        String result = userString.findMaximum( "Peach","Apple","Banana");
+        String result = userString.findMaximum( "Apple","Peach","Banana");
         boolean max = (result.equals("Peach")) ? true : false;
         Assertions.assertTrue(max);
     }
 
     @Test
-    public void givenMaxString_NotAtFirstPosition_ShouldReturnFalse(){
+    public void givenMaxString_NotAtSecondPosition_ShouldReturnFalse(){
         TestMaximum userString = new TestMaximum();
-        String result = userString.findMaximum("Apple","Peach","Banana");
-        boolean max = (result.equals("Apple")) ? true : false;
+        String result = userString.findMaximum("Apple","Banana","Peach");
+        boolean max = (result.equals("Banana")) ? true : false;
         Assertions.assertFalse(max);
     }
 }
