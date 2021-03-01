4fa873a9f2385f0564a874f384df8e3394ac3fcf
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-01 22:20:23 +0530

    Added : Created findMaximum method for checking max number which is present at third position

--- main/java/com/testmaximum/TestMaximum.java
+++ main/java/com/testmaximum/TestMaximum.java
@@ -10,11 +10,11 @@ public class TestMaximum {
     }
 
     public static Integer findMaximum(Integer xInt, Integer yInt, Integer zInt) {
-        Integer max = yInt;
+        Integer max = zInt;
         if(xInt.compareTo(max) > 0)
             max = xInt;
-        if(zInt.compareTo(max) > 0)
-            max = zInt;
+        if(yInt.compareTo(max) > 0)
+            max = yInt;
         return max;
     }
 }
