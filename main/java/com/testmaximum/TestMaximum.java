2721d898933676c21383347d62b0d942208c6548
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-01 21:58:53 +0530

    Tested : Test data has max number in first position

---
+++ main/java/com/testmaximum/TestMaximum.java
@@ -0,0 +1,21 @@
+package com.testmaximum;
+
+public class TestMaximum {
+    Integer xInt,yInt,zInt;
+
+    public TestMaximum() {
+        this.xInt = xInt;
+        this.yInt = yInt;
+        this.zInt = zInt;
+    }
+
+    public static Integer findMaximum(Integer xInt, Integer yInt, Integer zInt) {
+        Integer max = xInt;
+        if(yInt.compareTo(max) > 0)
+            max = yInt;
+        if(zInt.compareTo(max) > 0)
+            max = zInt;
+        return max;
+
+    }
+}
---
+++ test/java/com/testmaximum/TestMaximumTest.java
@@ -0,0 +1,22 @@
+package com.testmaximum;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestMaximumTest {
+    @Test
+    public void givenMaxNumber_AtFirstPosition_ShouldReturnTrue(){
+        TestMaximum userNumbers = new TestMaximum();
+        Integer result = userNumbers.findMaximum(3,2,1);
+        boolean max = (result==3) ? true : false;
+        Assertions.assertTrue(max);
+    }
+
+    @Test
+    public void givenMaxNumber_NotAtFirstPosition_ShouldReturnFalse(){
+        TestMaximum userNumbers = new TestMaximum();
+        Integer result = userNumbers.findMaximum(6,10,5);
+        boolean max = (result==6) ? true : false;
+        Assertions.assertFalse(max);
+    }
+}
