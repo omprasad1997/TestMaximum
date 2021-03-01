afc6520b73a0da2da52a9b3bcf2029f74305c620
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-02 00:56:16 +0530

    Tested : Wrote Junit test to test data has max string in third position

--- test/java/com/testmaximum/TestMaximumTest.java
+++ test/java/com/testmaximum/TestMaximumTest.java
@@ -5,17 +5,17 @@ import org.junit.jupiter.api.Test;
 
 public class TestMaximumTest {
     @Test
-    public void givenMaxString_AtSecondPosition_ShouldReturnTrue(){
+    public void givenMaxString_AtThirdPosition_ShouldReturnTrue(){
         TestMaximum userString = new TestMaximum();
-        String result = userString.findMaximum( "Apple","Peach","Banana");
+        String result = userString.findMaximum( "Apple","Banana","Peach");
         boolean max = (result.equals("Peach")) ? true : false;
         Assertions.assertTrue(max);
     }
 
     @Test
-    public void givenMaxString_NotAtSecondPosition_ShouldReturnFalse(){
+    public void givenMaxString_NotAtThirdPosition_ShouldReturnFalse(){
         TestMaximum userString = new TestMaximum();
-        String result = userString.findMaximum("Apple","Banana","Peach");
+        String result = userString.findMaximum("Apple","Peach","Banana");
         boolean max = (result.equals("Banana")) ? true : false;
         Assertions.assertFalse(max);
     }
